5269e37278a0de18b453e52f37dfb091a166cb1a
taa <adornettoluigi98@gmail.com>
2021-08-30 10:45:50 +0200

    added function to retrieve multiple conditions in base of the current device

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,9 @@
+/// IMPORTING THIRD PARTY PACKAGES
+import 'package:zwap_utils/zwap_utils.dart';
+
+/// IMPORTING LOCAL PACKAGES
+import 'package:taastrap/mediaQueries/mediaQueries.dart';
+
 /// Check if string is a numeric type
 bool isNumeric(String s) {
   return int.tryParse(s) != null;
@@ -11,3 +17,23 @@ Map<int, String> mappingSizes = {
   3: "lg",
   4: "xl"
 };
+
+/// It gets multiple conditions in base of which of the different devices is the current device
+dynamic getMultipleConditions(dynamic four, dynamic three, dynamic two, dynamic one, dynamic zero){
+  int deviceType = Utils.getIt<Generic>().deviceType();
+  if(deviceType == 4){
+    return four;
+  }
+  else if(deviceType == 3){
+    return three;
+  }
+  else if(deviceType == 2){
+    return two;
+  }
+  else if(deviceType == 1){
+    return one;
+  }
+  else{
+    return zero;
+  }
+}
